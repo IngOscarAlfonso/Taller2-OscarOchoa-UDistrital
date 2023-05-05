37635b0b30ac090e420f80d2e9844357a70686c5
DavincisDreamTeam <ing.oscar.alfonso@gmail.com>
2023-05-05 11:54:10 -0500

    Ajustes en el index.html y crear el evento click del form

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,22 @@
-function guardarEstudiante(){
-    console.log("Hola mundo");
-}
+const form = document.getElementById('form') as HTMLElement;
+const btnguardar = document.getElementById('btn-guardar') as HTMLElement;
+
+
+btnguardar.addEventListener('click', (e) => {
+    e.preventDefault();
+    console.log("Ingreso a la validación del Formulario");
+    //Hago la validación del formulario
+
+    //Obtener los datos de los input
+    const tipoDocumento = document.getElementById('tipoIdentificacion') as HTMLInputElement;
+    const numeroDocumento = document.getElementById('NumeroIdentificacion') as HTMLInputElement;
+    const nombres = document.getElementById('nombres') as HTMLInputElement;
+    const apellidos = document.getElementById('apellidos') as HTMLInputElement;
+    const celular = document.getElementById('celular') as HTMLInputElement;
+    const email = document.getElementById('email') as HTMLInputElement;
+    const linkedin = document.getElementById('linkedin') as HTMLInputElement;
+    const github = document.getElementById('github') as HTMLInputElement;
+
+    console.log(tipoDocumento.value);
+});
+
